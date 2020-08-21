3fb56a05a6b402100a8ee5d70c6a398579cf6ccf
ktcards <35540689+ktcards@users.noreply.github.com>
2020-08-20 21:18:12 -0500

    Update pi_image_processing.py

--- pi_image_processing.py
+++ pi_image_processing.py
@@ -1,5 +1,14 @@
-from piCamera import PiCamera
-import cv2
+from picamera import PiCamera
 from time import sleep
+import cv2
+import numpy as np
 
-camera=PiCamera()
+#camera preview for 5 second, then takes picture
+camera = PiCamera()
+camera.start_preview()
+sleep(5)
+camera.capture('pipic.jpg')
+camera.stop_preview()
+
+
+
